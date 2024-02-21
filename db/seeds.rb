5a937ee87ba307478cd77b523a9da36775a99196
Caroline Deuse <carolinedeuse@gmail.com>
2024-02-21 16:30:30 +0100

    challenge 3 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating fake flats..."
+
+6.times do
+  Flat.create!(
+    name: "#{Faker::Lorem.word} #{Faker::Lorem.word} #{["flat", "house", "appartment"].sample}",
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Lorem.paragraph(sentence_count: 2, random_sentences_to_add: 3),
+    price_per_night: rand(50..200),
+    number_of_guests: rand(1..6),
+    image: ["https://source.unsplash.com/random/?house", "https://source.unsplash.com/random/?home", "https://source.unsplash.com/random/?appartment"].sample
+  )
+end
+
+puts "Finished!"
